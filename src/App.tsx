6f9c309ec3b207b08afc95dc026bea6d9a987812
piotr <piotrek.priv.michalski@wp.pl>
2023-07-05 22:24:21 +0200

    float input fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import axios from "axios";
 import { ClipLoader } from "react-spinners";
 import { AiOutlineInfoCircle } from "react-icons/ai";
@@ -9,15 +9,21 @@ import Button from "./components/Button.tsx";
 import Input from "./components/Input.tsx";
 
 function App() {
-  const [usersInput, setUsersInput] = useState(0);
+  const [usersInput, setUsersInput] = useState("0.00");
   const [averageCourse, setAverageCourse] = useState<null | number>(null);
   const [isLoading, setIsLoading] = useState(false);
 
   const API_URL = "https://api.nbp.pl/api/exchangerates/rates/a";
   const IMG_URL = "https://purecatamphetamine.github.io/country-flag-icons/3x2";
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value;
 
-  function valueAfterSwap(afterDecimal = 2) {
-    return (averageCourse! * usersInput).toFixed(afterDecimal);
+    if (input.match(/^([0-9]{1,})?(\.)?([0-9]{1,})?$/)) setUsersInput(input);
+  };
+
+  function valueAfterSwap() {
+    if (isNaN(parseFloat(usersInput))) setAverageCourse(null);
+    return (averageCourse! * parseFloat(usersInput)).toFixed(2);
   }
   async function handleFormSubmit(e: FormEvent) {
     e.preventDefault();
@@ -46,10 +52,9 @@ function App() {
             <FormItem imgUrl={`${IMG_URL}/EU.svg`} currency={"EUR"} />
             <InputWrapper>
               <Input
-                onChange={(e) => setUsersInput(Number(e.target.value))}
-                value={usersInput.toFixed(2)}
                 className="h-full focus:outline-blue-400 pr-4 text-end font-medium text-lg"
-                type="number"
+                value={usersInput}
+                onChange={handleOnChange}
               />
             </InputWrapper>
           </FormGroup>
@@ -59,7 +64,7 @@ function App() {
               <Input
                 readOnly
                 value={averageCourse ? valueAfterSwap() : "0.00"}
-                type={"number"}
+                type={"text"}
               />
             </InputWrapper>
           </FormGroup>
@@ -67,7 +72,7 @@ function App() {
             <div className="self-end mt-6 flex flex-col items-end ">
               <div className="flex items-center  gap-1">
                 <AiOutlineInfoCircle size={23} />
-                <p>1 EUR = {averageCourse.toFixed(4)} PLN</p>
+                <p>1 EUR = {averageCourse} PLN</p>
               </div>
               <p className="text-sm">
                 1 PLN = {(1 / averageCourse).toFixed(4)} EUR
@@ -80,7 +85,7 @@ function App() {
         </form>
         {averageCourse && (
           <p className="pl-10 pb-10 text-sm">
-            Za {usersInput.toFixed(2)} EUR otrzymasz{" "}
+            Za {usersInput} EUR otrzymasz{" "}
             <strong>{valueAfterSwap()} PLN</strong>
           </p>
         )}
