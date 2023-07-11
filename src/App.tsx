ff27575fe4f1ef4c1cdcab80ea4606f87b8296d4
piotr <piotrek.priv.michalski@wp.pl>
2023-07-11 21:10:54 +0200

    pair programming

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,68 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import axios from "axios";
-import { ClipLoader } from "react-spinners";
-import { AiOutlineInfoCircle } from "react-icons/ai";
 import FormItem from "./components/FormItem.tsx";
 import FormGroup from "./components/FormGroup.tsx";
 import InputWrapper from "./components/InputWrapper.tsx";
-import Button from "./components/Button.tsx";
 import Input from "./components/Input.tsx";
+import { AiOutlineInfoCircle } from "react-icons/ai";
 
+export interface Currencies {
+  currency: string;
+  code: string;
+  mid: number;
+}
 function App() {
-  const [usersInput, setUsersInput] = useState("0.00");
+  const [firstInputValue, setFirstInputValue] = useState("0.00");
+  const [secondInputValue, setSecondInputValue] = useState("0.00");
   const [averageCourse, setAverageCourse] = useState<null | number>(null);
-  const [isLoading, setIsLoading] = useState(false);
 
-  const API_URL = "https://api.nbp.pl/api/exchangerates/rates/a";
-  const IMG_URL = "https://purecatamphetamine.github.io/country-flag-icons/3x2";
-  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [currencies, setCurrencies] = useState<Currencies[]>([]);
+  const [selectedCurrencyRate, setSelectedCurrencyRate] = useState<number>();
+
+  const API_URL = "https://api.nbp.pl/api/exchangerates/tables/A/?format=json";
+
+  async function getAllCurrencies() {
+    const res = await axios.get(API_URL);
+    const currencies: Currencies[] = res.data[0].rates;
+    currencies.sort((a, b) => (a.code > b.code ? 1 : -1));
+    setCurrencies(currencies);
+  }
+  useEffect(() => {
+    getAllCurrencies();
+  }, []);
+
+  function exchangeCurrency(inputValue, inputNumber) {
+    if (inputNumber === 1) {
+      setSecondInputValue(inputValue * selectedCurrencyRate);
+    } else {
+      setFirstInputValue(inputValue / selectedCurrencyRate);
+    }
+  }
+
+  const handleFirstOnChange = (e: ChangeEvent<HTMLInputElement>) => {
     const input = e.target.value;
 
-    if (input.match(/^([0-9]{1,})?(\.)?([0-9]{1,})?$/)) setUsersInput(input);
+    if (input.match(/^([0-9]{1,})?(\.)?([0-9]{1,})?$/)) {
+      setFirstInputValue(input);
+    }
+
+    exchangeCurrency(input, 1);
+  };
+
+  const handleSecondOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value;
+
+    if (input.match(/^([0-9]{1,})?(\.)?([0-9]{1,})?$/)) {
+      setSecondInputValue(input);
+    }
+
+    exchangeCurrency(input, 2);
   };
 
   function valueAfterSwap() {
-    if (isNaN(parseFloat(usersInput))) setAverageCourse(null);
-    return (averageCourse! * parseFloat(usersInput)).toFixed(2);
+    if (isNaN(parseFloat(firstInputValue))) setAverageCourse(null);
+    return (averageCourse! * parseFloat(firstInputValue)).toFixed(2);
   }
   async function handleFormSubmit(e: FormEvent) {
     e.preventDefault();
@@ -41,7 +80,7 @@ function App() {
   return (
     <main className="min-h-screen flex flex-col justify-center items-center">
       <h1 className="text-4xl md:text-5xl  font-medium mb-20">
-        Kalkulator euro na złoty
+        Kalkulator wielowalutowy
       </h1>
       <section className="bg-gray-100">
         <form
@@ -49,46 +88,48 @@ function App() {
           className="flex flex-col gap-6 rounded-lg p-8"
         >
           <FormGroup>
-            <FormItem imgUrl={`${IMG_URL}/EU.svg`} currency={"EUR"} />
+            <FormItem
+              currencies={currencies}
+              setSelectedCurrency={setSelectedCurrencyRate}
+            />
             <InputWrapper>
               <Input
                 className="h-full focus:outline-blue-400 pr-4 text-end font-medium text-lg"
-                value={usersInput}
-                onChange={handleOnChange}
+                value={firstInputValue}
+                onChange={handleFirstOnChange}
               />
             </InputWrapper>
           </FormGroup>
           <FormGroup>
-            <FormItem imgUrl={`${IMG_URL}/PL.svg`} currency={"PLN"} />
+            <div className=" p-6 flex gap-4 items-center">
+              <span className="font-medium text-xl">PLN</span>
+            </div>
             <InputWrapper>
               <Input
-                readOnly
-                value={averageCourse ? valueAfterSwap() : "0.00"}
+                onChange={handleSecondOnChange}
+                value={secondInputValue}
                 type={"text"}
               />
             </InputWrapper>
           </FormGroup>
-          {averageCourse && (
+          {selectedCurrencyRate && (
             <div className="self-end mt-6 flex flex-col items-end ">
               <div className="flex items-center  gap-1">
                 <AiOutlineInfoCircle size={23} />
-                <p>1 EUR = {averageCourse} PLN</p>
+                <p>1 aktualna waluta = {selectedCurrencyRate} PLN</p>
               </div>
               <p className="text-sm">
-                1 PLN = {(1 / averageCourse).toFixed(4)} EUR
+                1 PLN = {(1 / selectedCurrencyRate).toFixed(4)} aktualna waluta
               </p>
             </div>
           )}
-          <Button disabled={isLoading}>
-            {isLoading ? <ClipLoader color={"white"} /> : "Sprawdź kurs"}
-          </Button>
         </form>
-        {averageCourse && (
-          <p className="pl-10 pb-10 text-sm">
-            Za {usersInput} EUR otrzymasz{" "}
-            <strong>{valueAfterSwap()} PLN</strong>
-          </p>
-        )}
+        {/*{averageCourse && (*/}
+        {/*  <p className="pl-10 pb-10 text-sm">*/}
+        {/*    Za {firstInputValue} EUR otrzymasz{" "}*/}
+        {/*    <strong>{valueAfterSwap()} PLN</strong>*/}
+        {/*  </p>*/}
+        {/*)}*/}
       </section>
     </main>
   );
--- src/components/FormItem.tsx
+++ src/components/FormItem.tsx
@@ -1,18 +1,26 @@
+import { Currencies } from "../App.tsx";
+
 interface FormItemProps {
-  imgUrl: string;
-  currency: "PLN" | "EUR";
+  currencies: Currencies[];
+  setSelectedCurrency: React.Dispatch<
+    React.SetStateAction<Currencies | undefined>
+  >;
 }
-const FormItem = ({ imgUrl, currency }: FormItemProps) => {
+const FormItem = ({ currencies, setSelectedCurrency }: FormItemProps) => {
+  function handleOnChange(e) {
+    setSelectedCurrency(Number(e.target.value));
+  }
   return (
     <div className=" p-6 flex gap-4 items-center">
-      <img
-        className="shadow rounded"
-        width={50}
-        height={50}
-        alt={currency}
-        src={imgUrl}
-      />
-      <span className="font-medium text-xl">{currency}</span>
+      <select onChange={handleOnChange} name="currencies" id="currencies">
+        {currencies.map((currency) => {
+          return (
+            <option key={currency.code} value={currency.mid}>
+              {currency.code}
+            </option>
+          );
+        })}
+      </select>
     </div>
   );
 };
